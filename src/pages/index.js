abea897d5c4cf0b368370166cdd3d3631abd6cd4
Michael Durkin <mdurks@gmail.com>
2020-07-15 11:23:19 +0100

    First commit, pulls product names

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,22 +1,51 @@
 import React from "react"
-import { Link } from "gatsby"
-
-import Layout from "../components/layout"
-import Image from "../components/image"
-import SEO from "../components/seo"
+import { StaticQuery } from "gatsby"
 
 const IndexPage = () => (
-  <Layout>
-    <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
-    </div>
-    <Link to="/page-2/">Go to page 2</Link> <br />
-    <Link to="/using-typescript/">Go to "Using TypeScript"</Link>
-  </Layout>
+  <StaticQuery
+    query={graphql`
+      query {
+        gcms {
+          products {
+            name
+          }
+        }
+      }
+    `}
+    render={data => (
+      <div>
+        <ul>
+          {data.gcms.products.map(mountain => {
+            const { name } = mountain
+            return <li>{name}</li>
+          })}
+        </ul>
+      </div>
+    )}
+  />
 )
 
 export default IndexPage
+
+// import React from "react"
+// import { Link } from "gatsby"
+
+// import Layout from "../components/layout"
+// import Image from "../components/image"
+// import SEO from "../components/seo"
+
+// const IndexPage = () => (
+//   <Layout>
+//     <SEO title="Home" />
+//     <h1>Hi people</h1>
+//     <p>Welcome to your new Gatsby site.</p>
+//     <p>Now go build something great.</p>
+//     <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
+//       <Image />
+//     </div>
+//     <Link to="/page-2/">Go to page 2</Link> <br />
+//     <Link to="/using-typescript/">Go to "Using TypeScript"</Link>
+//   </Layout>
+// )
+
+// export default IndexPage
