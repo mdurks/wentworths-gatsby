2f0a00c03c3faf05a5d3e8c00b1f3178acc137ba
Michael Durkin <mdurks@gmail.com>
2020-07-15 19:42:41 +0100

    setTimeout on Snipcart

--- src/components/snipcart.js
+++ src/components/snipcart.js
@@ -2,16 +2,18 @@ import React from "react"
 
 class Snipcart extends React.Component {
   componentDidMount() {
-    var snipcartCSS = document.createElement("link")
-    snipcartCSS.rel = "stylesheet"
-    snipcartCSS.href =
-      "https://cdn.snipcart.com/themes/v3.0.17/default/snipcart.css"
-    document.head.appendChild(snipcartCSS)
+    setTimeout(() => {
+      var snipcartCSS = document.createElement("link")
+      snipcartCSS.rel = "stylesheet"
+      snipcartCSS.href =
+        "https://cdn.snipcart.com/themes/v3.0.17/default/snipcart.css"
+      document.head.appendChild(snipcartCSS)
 
-    var snipcartScript = document.createElement("script")
-    snipcartScript.src =
-      "https://cdn.snipcart.com/themes/v3.0.17/default/snipcart.js"
-    document.body.appendChild(snipcartScript)
+      var snipcartScript = document.createElement("script")
+      snipcartScript.src =
+        "https://cdn.snipcart.com/themes/v3.0.17/default/snipcart.js"
+      document.body.appendChild(snipcartScript)
+    }, 4000)
   }
   render() {
     return (
