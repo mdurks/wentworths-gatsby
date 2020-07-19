591d22a7527d1e2fb60a030d9c459b0ef4b79417
Michael Durkin <mdurks@gmail.com>
2020-07-19 10:48:40 +0100

    Move snipcart into Layout to test immediate loading

--- src/components/layout.js
+++ src/components/layout.js
@@ -33,6 +33,17 @@ const Layout = ({ children }) => {
       <MainNav />
       <main>{children}</main>
       <Footer />
+      <link
+        rel="stylesheet"
+        href="https://cdn.snipcart.com/themes/v3.0.17/default/snipcart.css"
+      />
+      <div
+        hidden
+        id="snipcart"
+        data-api-key="YmJkMDViNmYtYjg2MC00YTc0LTkwNDMtNGUzZGYwYTkyN2JjNjM3MzAzMTM2MDgxMjQzNzc0"
+        data-currency="gbp"
+      ></div>
+      <script src="https://cdn.snipcart.com/themes/v3.0.17/default/snipcart.js"></script>
     </>
   )
 }
