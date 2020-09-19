20166f9cb34ba6afb4ff70f4e437c80e32e30034
Michael Durkin <mdurks@gmail.com>
2020-09-19 16:23:54 +0100

    Re-started implementing Snipcart - simple imbed at bottom of page method

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -6,34 +6,11 @@
 
 // You can delete this file if you're not using it
 
-// import React from "react"
-// export const onRenderBody = ({ setPostBodyComponents }, pluginOptions) => {
-//   setPostBodyComponents([
-//     <div
-//       hidden
-//       id="snipcart"
-//       data-api-key="YmJkMDViNmYtYjg2MC00YTc0LTkwNDMtNGUzZGYwYTkyN2JjNjM3MzAzMTM2MDgxMjQzNzc0"
-//       data-currency="gbp"
-//     ></div>,
-//     // <link
-//     //   rel="stylesheet"
-//     //   href="https://cdn.snipcart.com/themes/v3.0.17/default/snipcart.css"
-//     // />,
-//     // <script src="https://cdn.snipcart.com/themes/v3.0.17/default/snipcart.js"></script>,
-//     <script>
-//       var snipcartCSS = document.createElement("link"); snipcartCSS.rel =
-//       "stylesheet"; snipcartCSS.href =
-//       "https://cdn.snipcart.com/themes/v3.0.17/default/snipcart.css";
-//       document.head.appendChild(snipcartCSS); var snipcartScript =
-//       document.createElement("script"); snipcartScript.src =
-//       "https://cdn.snipcart.com/themes/v3.0.17/default/snipcart.js";
-//       document.body.appendChild(snipcartScript);
-//     </script>,
-//   ])
-// }
-
 import React from "react"
-export const onRenderBody = ({ setHeadComponents }, pluginOptions) => {
+export const onRenderBody = (
+  { setHeadComponents, setPostBodyComponents },
+  pluginOptions
+) => {
   setHeadComponents([
     <link rel="preconnect" href="https://fonts.gstatic.com/" />,
     <link rel="preconnect" href="https://app.snipcart.com" />,
@@ -42,5 +19,43 @@ export const onRenderBody = ({ setHeadComponents }, pluginOptions) => {
       href="https://fonts.googleapis.com/css2?family=Playfair+Display:ital@0;1&family=Raleway:wght@400;700&display=swap"
       rel="stylesheet"
     ></link>,
+  ])
+
+  setPostBodyComponents([
+    <div
+      hidden
+      id="snipcart"
+      data-api-key="YmJkMDViNmYtYjg2MC00YTc0LTkwNDMtNGUzZGYwYTkyN2JjNjM3MzAzMTM2MDgxMjQzNzc0"
+      data-currency="gbp"
+    ></div>,
+    // <link
+    //   rel="stylesheet"
+    //   href="https://cdn.snipcart.com/themes/v3.0.17/default/snipcart.css"
+    // />,
+    <link
+      rel="stylesheet"
+      href="https://cdn.snipcart.com/themes/v3.0.17/default/snipcart.css"
+      media="print"
+      onload="this.media='all'"
+    />,
+    <noscript>
+      <link
+        rel="stylesheet"
+        href="https://cdn.snipcart.com/themes/v3.0.17/default/snipcart.css"
+      />
+    </noscript>,
+    <script
+      async
+      src="https://cdn.snipcart.com/themes/v3.0.17/default/snipcart.js"
+    ></script>,
+    // <script>
+    //   var snipcartCSS = document.createElement("link"); snipcartCSS.rel =
+    //   "stylesheet"; snipcartCSS.href =
+    //   "https://cdn.snipcart.com/themes/v3.0.17/default/snipcart.css";
+    //   document.head.appendChild(snipcartCSS); var snipcartScript =
+    //   document.createElement("script"); snipcartScript.src =
+    //   "https://cdn.snipcart.com/themes/v3.0.17/default/snipcart.js";
+    //   document.body.appendChild(snipcartScript);
+    // </script>,
   ])
 }
