ea112e39a151db3ab85f20f2674768cd37522eb7
mdurks <mdurks@gmail.com>
2021-04-06 21:10:42 +0100

    image text box - created GSAP animations on scrollTrigger

--- src/components/image_text_box.js
+++ src/components/image_text_box.js
@@ -1,18 +1,23 @@
 import React from "react"
+import { useRef, useEffect, useState, createRef } from "react"
 import { graphql, useStaticQuery } from "gatsby"
 import styled from "styled-components"
 import { Styled_SiteContainer } from "../styles/commonStyles"
+import { gsap, ScrollTrigger } from "gsap/all"
 
 import white_bordered_two_ringss_pink_ribbon from "../images/general-product/white-bordered-two-ringss-pink-ribbon.jpg"
 import white_bordered_pink_diamon_ring from "../images/general-product/white-bordered-pink-diamon-ring.jpg"
 import sparkle_background_blue from "../images/sparkle-background-blue.png"
+
+gsap.registerPlugin(ScrollTrigger)
+gsap.core.globals("ScrollTrigger", ScrollTrigger)
 
 const bp_min_tablet = "@media (min-width: 768px)"
 const bp_min_desktop = "@media (min-width: 1024px)"
 const bp_max_desktop = "@media (max-width: 1024px)"
 const bp_desktop_max = "1400px"
 
-const section_vertical_padding = "12vh"
+const section_vertical_padding = "0vh"
 const section_horizontal_padding = "50px"
 const section_content_max_width = "1800px"
 
@@ -43,6 +48,7 @@ const Div__ITB = styled.div`
   justify-content: center;
   align-items: center;
   padding: 15px 0 75px;
+  overflow: hidden;
 
   ${bp_min_desktop} {
     min-height: calc(100vh + ${section_vertical_padding});
@@ -125,17 +131,19 @@ const Img__ITB__imgSparkle = styled.img`
   }
 `
 
-const Img__ITB__layout1__img1 = styled.img`
+const Img__ITB__layout1__img1 = styled.div`
   display: none;
   ${bp_min_desktop} {
     display: block;
     position: absolute;
     left: calc(40% - (326px / 2));
     top: calc(50% - (444px / 2));
+    height: 444px;
+    overflow: hidden;
   }
 `
 
-const Img__ITB__layout1__img2 = styled.img`
+const Img__ITB__layout1__img2 = styled.div`
   margin: 20px 0 0 -5px;
 
   ${bp_min_desktop} {
@@ -143,6 +151,8 @@ const Img__ITB__layout1__img2 = styled.img`
     position: absolute;
     left: calc(59% - (356px / 2));
     top: calc(95% - (211px / 2));
+    overflow: hidden;
+    height: 211px;
     z-index: 1;
   }
 `
@@ -158,30 +168,135 @@ const ImageTextBox = () => {
     gcms: { welcomes },
   } = useStaticQuery(pageQuery)
 
+  let tl = gsap.timeline()
+  let gsap__Div__ITB = null
+  let gsap__ITB__siteContainer = null
+  let gsap__title = null
+  let gsap__introMessage = null
+  let gsap__colorBox01 = null
+  let gsap__Img__ITB__imgSparkle = null
+  let gsap__Img__ITB__layout1__img1 = null
+  let gsap__Img__ITB__layout1__img2 = null
+
+  useEffect(() => {
+    gsap.from(gsap__title, {
+      scrollTrigger: {
+        trigger: gsap__title,
+        // markers: true,
+        start: "top 80%",
+      },
+      duration: 2,
+      opacity: 0,
+      x: 20,
+    })
+
+    gsap.from(gsap__introMessage, {
+      scrollTrigger: {
+        trigger: gsap__introMessage,
+        // markers: true,
+        start: "top 80%",
+      },
+      duration: 2,
+      opacity: 0,
+      x: 20,
+    })
+
+    gsap.from(gsap__colorBox01, {
+      scrollTrigger: {
+        trigger: gsap__colorBox01,
+        // markers: true,
+        //start: "top 90%", // colorBox, viewport start location
+        start: 0,
+        end: () => gsap__Div__ITB.offsetTop * 0.9,
+        scrub: true,
+      },
+      duration: 2,
+      opacity: 0,
+      y: gsap__ITB__siteContainer.offsetTop,
+    })
+
+    gsap.from(gsap__Img__ITB__imgSparkle, {
+      scrollTrigger: {
+        trigger: gsap__Img__ITB__imgSparkle,
+        // markers: true,
+        //start: "top 90%", // colorBox, viewport start location
+        start: 0,
+        end: () => gsap__Div__ITB.offsetTop * 0.9,
+        scrub: true,
+      },
+      duration: 2,
+      y: "+=500",
+    })
+
+    gsap.from(gsap__Img__ITB__layout1__img1, {
+      scrollTrigger: {
+        trigger: gsap__Img__ITB__layout1__img1,
+        // markers: true,
+        //start: "top 90%", // colorBox, viewport start location
+        start: "-100 55%",
+        end: () => gsap__Div__ITB.offsetTop * 0.9,
+        // scrub: true,
+      },
+      duration: 2,
+      y: "+=50",
+      height: 0,
+      opacity: 0,
+    })
+
+    gsap.from(gsap__Img__ITB__layout1__img2, {
+      scrollTrigger: {
+        trigger: gsap__Img__ITB__layout1__img2,
+        // markers: true,
+        //start: "top 90%", // colorBox, viewport start location
+        start: "-100 60%",
+        end: () => gsap__Div__ITB.offsetTop * 0.9,
+        // scrub: true,
+      },
+      duration: 2,
+      y: "+=50",
+      height: 0,
+      opacity: 0,
+    })
+
+    // return function to kill timeline on dismount
+    return () => tl.kill()
+  }, [])
+
   return (
     <>
-      <Div__ITB>
-        <Styled_SiteContainer>
+      <Div__ITB ref={e => (gsap__Div__ITB = e)}>
+        <Styled_SiteContainer ref={e => (gsap__ITB__siteContainer = e)}>
           <Div__ITB__textGroup>
-            <h1>{welcomes[0].heroHeading}</h1>
+            <h1 className="gsap__title" ref={e => (gsap__title = e)}>
+              {welcomes[0].heroHeading}
+            </h1>
             <div
               dangerouslySetInnerHTML={{
                 __html: welcomes[0].firstIntroMessage.html,
               }}
+              ref={e => (gsap__introMessage = e)}
             ></div>
           </Div__ITB__textGroup>
           <Div__ITB__imgGroup>
-            <Div__ITB__colorBox01></Div__ITB__colorBox01>
+            <Div__ITB__colorBox01
+              ref={e => (gsap__colorBox01 = e)}
+            ></Div__ITB__colorBox01>
             <Div__ITB__colorBox02></Div__ITB__colorBox02>
-            <Img__ITB__imgSparkle src={sparkle_background_blue} alt="" />
+            <Img__ITB__imgSparkle
+              ref={e => (gsap__Img__ITB__imgSparkle = e)}
+              src={sparkle_background_blue}
+              alt=""
+            />
             <Img__ITB__layout1__img1
-              src={white_bordered_two_ringss_pink_ribbon}
-              alt=""
-            />
+              ref={e => (gsap__Img__ITB__layout1__img1 = e)}
+            >
+              <img src={white_bordered_two_ringss_pink_ribbon} alt="" />
+            </Img__ITB__layout1__img1>
             <Img__ITB__layout1__img2
-              src={white_bordered_pink_diamon_ring}
-              alt=""
-            />
+              ref={e => (gsap__Img__ITB__layout1__img2 = e)}
+            >
+              <img src={white_bordered_pink_diamon_ring} alt="" />
+            </Img__ITB__layout1__img2>
           </Div__ITB__imgGroup>
         </Styled_SiteContainer>
       </Div__ITB>
