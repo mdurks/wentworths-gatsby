f0ed403fb3872bbc091f25b4aac63811d1a384f7
mdurks <mdurks@gmail.com>
2021-04-11 23:04:55 +0100

    Block bespoke design advert - created scrolling gsap timeline of polaroids right to left.

--- src/components/block_bespoke_design_advert.js
+++ src/components/block_bespoke_design_advert.js
@@ -1,27 +1,32 @@
 import React from "react"
+import { useEffect } from "react"
 import styled from "styled-components"
 import { Styled_SiteContainer } from "../styles/commonStyles"
+import { gsap, ScrollTrigger } from "gsap/all"
 
 import img_sketch_rings from "../images/misc/sketch-rings.jpg"
 import img_sketch_book from "../images/misc/sketch-book.jpg"
+
+gsap.registerPlugin(ScrollTrigger)
+gsap.core.globals("ScrollTrigger", ScrollTrigger)
 
 const bp_max_mobile = "@media (max-width: 767px)"
 const bp_min_desktop = "@media (min-width: 1024px)"
 const bp_max_desktop = "@media (max-width: 1024px)"
 const bp_desktop_max = "1400px"
 
-const section_vertical_padding = "12vh"
+const section_vertical_padding = "0vh"
 const section_horizontal_padding = "50px"
 const section_content_max_width = "1800px"
 
 const Div__block_bespoke_design_advert = styled.div`
+  position: relative;
   padding: (${section_vertical_padding} / 2) ${section_horizontal_padding};
   min-height: calc(100vh + ${section_vertical_padding});
-  position: relative;
   display: flex;
   justify-content: center;
   align-items: center;
-  padding: 75px 0;
+  /* padding: 75px 0; */
   text-align: center;
   background-color: #e5e3de;
 
@@ -77,6 +82,47 @@ const Div__block_bespoke_design_advert = styled.div`
     }
   }
 
+  // -------------------------------
+
+  // GSAP Animation
+
+  .bespoke_design_gsap_scrolling_image_container {
+    position: absolute;
+    width: calc(100vw - 20px);
+    height: 100vh;
+    /* outline: 1px solid blue; */
+    overflow: hidden;
+    perspective: 1400px;
+  }
+  .offscreen_right {
+    position: absolute;
+    left: 105%;
+    top: 0;
+    height: 287px;
+    width: 287px;
+    transform-style: preserve-3d;
+
+    .gsap_scrolling_img {
+      height: 287px;
+      width: 287px;
+      border: 10px solid white;
+      /* box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25); */
+    }
+
+    > div {
+      content: " ";
+      position: absolute;
+      top: 0;
+      width: 287px;
+      height: 287px;
+      /* background-color: white; */
+      background: linear-gradient(to bottom, #fdfbf7 0%, #cccac5 100%);
+      transform: rotateY(180deg);
+    }
+  }
+
+  // -------------------------------
+
   .bespoke_design_heading1 {
     position: relative;
     width: 100%;
@@ -102,8 +148,8 @@ const Div__block_bespoke_design_advert = styled.div`
     line-height: clamp(50px, 4vw, 30px);
     font-family: "Playfair Display", serif;
     letter-spacing: -1px;
-    color: #b49372;
-    text-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
+    /* color: #b49372; */
+    text-shadow: 0px 3px 3px rgba(0, 0, 0, 0.35);
 
     ${bp_min_desktop} {
       width: auto;
@@ -158,10 +204,164 @@ const Div__block_bespoke_design_advert = styled.div`
 `
 
 const Block_bespoke_design_advert = () => {
+  function getRndInteger(min, max) {
+    return Math.floor(Math.random() * (max - min)) + min
+  }
+
+  let gsap__sketch1 = null
+  let gsap__sketch2 = null
+  let gsap__sketch3 = null
+  let gsap__sketch4 = null
+  let gsap__sketch5 = null
+  let gsap__sketch6 = null
+  let gsap__sketch7 = null
+  let gsap__sketch8 = null
+  let gsap__sketch9 = null
+  let gsap__sketch10 = null
+  let gsap__block_bespoke_design_advert = null
+
+  useEffect(() => {
+    let viewportWidth = window.innerWidth
+    let viewportHeight = window.innerHeight
+    let gsap__sketch_group_yZone = viewportHeight
+    let gsap__sketch_group = [
+      gsap__sketch1,
+      gsap__sketch2,
+      gsap__sketch3,
+      gsap__sketch4,
+      gsap__sketch5,
+      gsap__sketch6,
+      gsap__sketch7,
+      gsap__sketch8,
+      gsap__sketch9,
+      gsap__sketch10,
+    ]
+
+    let tl_gsap__sketch_playreel = gsap.timeline({
+      scrollTrigger: {
+        trigger: gsap__block_bespoke_design_advert,
+        start: "top top",
+        // toggleActions: "play none none reset",
+        // markers: true,
+        // end: `+=${viewportWidth}`,
+        scrub: true,
+        pin: true,
+        anticipatePin: 1,
+      },
+    })
+    gsap__sketch_group.forEach((el, index) => {
+      gsap.set(el, {
+        y: getRndInteger(viewportHeight * 0.1, (viewportHeight - 300) * 0.9),
+        rotationX: 10,
+        rotationY: getRndInteger(-350, 10),
+        rotationZ: 353,
+      })
+      tl_gsap__sketch_playreel.to(
+        gsap__sketch_group[index],
+        {
+          left: "-300px",
+          duration: getRndInteger(7, 12),
+          ease: "ease",
+          rotationY: 350,
+        },
+        "-=" + getRndInteger(10, 20)
+      )
+    })
+    // tl_gsap__sketch_playreel.to(gsap__sketch1, {
+    //   left: "-300px",
+    //   duration: 10,
+    //   ease: "ease",
+    // })
+  }, [])
+
   return (
     <>
-      <Div__block_bespoke_design_advert>
+      <Div__block_bespoke_design_advert
+        ref={e => (gsap__block_bespoke_design_advert = e)}
+      >
         <Styled_SiteContainer>
+          <div className="bespoke_design_gsap_scrolling_image_container">
+            <div className="offscreen_right" ref={e => (gsap__sketch1 = e)}>
+              <img
+                className="gsap_scrolling_img"
+                src={img_sketch_rings}
+                alt=""
+              />
+              <div></div>
+            </div>
+            <div className="offscreen_right" ref={e => (gsap__sketch2 = e)}>
+              <img
+                className="gsap_scrolling_img"
+                src={img_sketch_rings}
+                alt=""
+              />
+              <div></div>
+            </div>
+            <div className="offscreen_right" ref={e => (gsap__sketch3 = e)}>
+              <img
+                className="gsap_scrolling_img"
+                src={img_sketch_rings}
+                alt=""
+              />
+              <div></div>
+            </div>
+            <div className="offscreen_right" ref={e => (gsap__sketch4 = e)}>
+              <img
+                className="gsap_scrolling_img"
+                src={img_sketch_rings}
+                alt=""
+              />
+              <div></div>
+            </div>
+            <div className="offscreen_right" ref={e => (gsap__sketch5 = e)}>
+              <img
+                className="gsap_scrolling_img"
+                src={img_sketch_rings}
+                alt=""
+              />
+              <div></div>
+            </div>
+            <div className="offscreen_right" ref={e => (gsap__sketch6 = e)}>
+              <img
+                className="gsap_scrolling_img"
+                src={img_sketch_rings}
+                alt=""
+              />
+              <div></div>
+            </div>
+            <div className="offscreen_right" ref={e => (gsap__sketch7 = e)}>
+              <img
+                className="gsap_scrolling_img"
+                src={img_sketch_rings}
+                alt=""
+              />
+              <div></div>
+            </div>
+            <div className="offscreen_right" ref={e => (gsap__sketch8 = e)}>
+              <img
+                className="gsap_scrolling_img"
+                src={img_sketch_rings}
+                alt=""
+              />
+              <div></div>
+            </div>
+            <div className="offscreen_right" ref={e => (gsap__sketch9 = e)}>
+              <img
+                className="gsap_scrolling_img"
+                src={img_sketch_rings}
+                alt=""
+              />
+              <div></div>
+            </div>
+            <div className="offscreen_right" ref={e => (gsap__sketch10 = e)}>
+              <img
+                className="gsap_scrolling_img"
+                src={img_sketch_rings}
+                alt=""
+              />
+              <div></div>
+            </div>
+          </div>
           <img className="img_sketch_rings" src={img_sketch_rings} alt="" />
           <img className="img_sketch_book" src={img_sketch_book} alt="" />
           <p className="bespoke_design_heading1">Bespoke design</p>
--- src/components/block_one_row_jewellery.js
+++ src/components/block_one_row_jewellery.js
@@ -1,6 +1,6 @@
 import React from "react"
 import { useEffect } from "react"
-import { gsap, ScrollTrigger, Elastic } from "gsap/all"
+import { gsap, ScrollTrigger } from "gsap/all"
 
 import styled from "styled-components"
 import { Styled_SiteContainer } from "../styles/commonStyles"
