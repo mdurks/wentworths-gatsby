879c9447f1fe170a437219e6d83890182526ceb4
mdurks <mdurks@gmail.com>
2021-04-29 10:19:42 +0100

    Block on row jewellery - process product and create animations for each on one scrollTrigger - needs clean up

--- src/components/block_one_row_jewellery.js
+++ src/components/block_one_row_jewellery.js
@@ -263,40 +263,143 @@ const Block_one_row_jewellery = () => {
         })
       })
     } else {
-      let tl_gsap__productItem = gsap.timeline({
-        paused: true,
-        scrollTrigger: {
-          trigger: Block_one_row_jewellery,
-          // markers: true,
-          start: "top 35%",
-          toggleActions: "play none none reset",
-        },
-      })
       //
-      let product_items = document.querySelectorAll(".productItem")
       //
-      tl_gsap__productItem.from(
-        return_array_center_out(document.querySelectorAll(".productItem")),
-        {
-          duration: 1.2,
-          opacity: 0,
-          // y: 120,
-          y: function (i) {
-            return "+=" + (product_items.length - i) * 100
-          },
-          ease: "back",
-          stagger: 0.15,
+      var toNodeList = function (arrayOfNodes) {
+        arrayOfNodes.forEach(function (item) {
+          item.className = item.className + " custom_class"
+        })
+
+        var elems = document.querySelectorAll(".custom_class")
+
+        arrayOfNodes.forEach(function (item) {
+          item.className = item.className.replace("custom_class", "")
+        })
+
+        return elems
+      }
+
+      function isNodeList(nodes) {
+        var stringRepr = Object.prototype.toString.call(nodes)
+
+        return (
+          typeof nodes === "object" &&
+          /^\[object (HTMLCollection|NodeList|Object)\]$/.test(stringRepr) &&
+          nodes.hasOwnProperty("length") &&
+          (nodes.length === 0 ||
+            (typeof nodes[0] === "object" && nodes[0].nodeType > 0))
+        )
+      }
+      //
+      //
+      // grab each individual product and create array of arrays
+      // divided by how many there are on a row
+      //
+      // turn:
+      // [1,2,3,4,5,6,7,8,9,10,11,12,13]
+      // into:
+      // [[1,2,3,4,5], [6,7,8,9,10], [11,12,13]]
+      //
+      let items = []
+      for (
+        let i = 1;
+        i <= document.querySelectorAll(".productItem").length;
+        i++
+      ) {
+        items.push(i)
+      }
+      let group_size = 5
+      let number_of_divisions = Math.ceil(items.length / group_size) // round up: 13 / 5 = 2.6 = 3
+      let items_divided_up = []
+
+      let slice_start = 0
+      let slice_end = group_size
+
+      for (let i = 0; i < number_of_divisions; i++) {
+        items_divided_up.push(items.slice(slice_start, slice_end))
+        slice_start += group_size
+        slice_end += group_size
+      }
+      //
+      // take this array of arrays and sort each group 'centered out'
+      //
+      let items_centered_out = []
+      //
+      for (let i = 0; i < items_divided_up.length; i++) {
+        items_centered_out.push(return_array_center_out(items_divided_up[i]))
+      }
+      // we now have:
+      // [[3,4,2,5,1], [8,9,7,10,6], [12,13,11]]
+      //
+      //
+      // Create timelines:
+      //
+      let tl_gsap__productItem = []
+      //
+      for (let x = 0; x < items_centered_out.length; x++) {
+        //
+        // tl_gsap__productItem[x] = gsap.timeline({
+        //   paused: true,
+        //   scrollTrigger: {
+        //     // trigger: Block_one_row_jewellery,
+        //     trigger: document.querySelector(
+        //       ".productItem" + items_centered_out[x][0]
+        //     ),
+        //     markers: true,
+        //     start: "-100 75%",
+        //     toggleActions: "play none none reset",
+        //   },
+        // })
+        //
+        let mylength = [...items_centered_out[x]]
+        //
+        for (let i = 0; i < mylength.length; i++) {
+          //
+          let currentNumber = items_centered_out[x][i]
+          let delayAmount = i * 0.15
+          let yAmount = (mylength.length - i) * 0.2 * 400
+          //
+          gsap.from(".productItem" + currentNumber, {
+            scrollTrigger: {
+              trigger: document.querySelector(".productItem" + currentNumber)
+                .parentElement,
+              // markers: true,
+              start: "-60 75%",
+              toggleActions: "play none none reset",
+            },
+            delay: delayAmount,
+            duration: 1.25,
+            opacity: 0,
+            y: yAmount,
+            ease: "back",
+          })
         }
-      )
-      tl_gsap__productItem.from(
-        return_array_center_out(document.querySelectorAll(".productDesc")),
-        {
-          duration: 0.25,
-          opacity: 0,
-          stagger: 0.2,
-        },
-        "-=.75"
-      )
+        //
+        //let product_items = document.querySelectorAll(".productItem")
+        //
+        // tl_gsap__productItem[x].from(toNodeList(items_centered_out[x]), {
+        //   duration: 2.2,
+        //   opacity: 0,
+        //   // y: 120,
+        //   y: function (i) {
+        //     return "+=" + (items_centered_out[x].length - i) * 100
+        //   },
+        //   ease: "back",
+        //   stagger: 0.15,
+        // })
+        // tl_gsap__productItem[x].from(
+        //   toNodeList(items_centered_out[x]),
+        //   {
+        //     duration: 0.25,
+        //     opacity: 0,
+        //     stagger: 0.2,
+        //   },
+        //   "-=.75"
+        // )
+        //
+      }
+      //
+      //
     }
 
     // return function to kill timeline on dismount
@@ -315,7 +418,7 @@ const Block_one_row_jewellery = () => {
           {/* Extra div added below because gatsby would not apply the styled component when building, works when wrappd in another div ?/?? */}
           <div>
             <Div__productRow>
-              <a href="/" className="productItem">
+              <a href="/" className="productItem productItem1">
                 <img
                   className="productImg"
                   src={img_product_ring_silver_round_blue_diamond}
@@ -325,7 +428,16 @@ const Block_one_row_jewellery = () => {
                 <p className="productDesc">Silver 1.2ct Round Blue Diamond</p>
               </a>
 
-              <a href="/" className="productItem">
+              <a href="/" className="productItem productItem2">
+                <img
+                  className="productImg"
+                  src={img_product_ring_silver_round_blue_diamond}
+                  alt=""
+                />
+                <div className="productStage"></div>
+                <p className="productDesc">Silver 1.2ct Round Blue Diamond</p>
+              </a>
+              <a href="/" className="productItem productItem3">
                 <img
                   className="productImg"
                   src={img_product_ring_silver_round_blue_diamond}
@@ -335,7 +447,36 @@ const Block_one_row_jewellery = () => {
                 <p className="productDesc">Silver 1.2ct Round Blue Diamond</p>
               </a>
 
-              <a href="/" className="productItem">
+              <a href="/" className="productItem productItem4">
+                <img
+                  className="productImg"
+                  src={img_product_ring_silver_round_blue_diamond}
+                  alt=""
+                />
+                <div className="productStage"></div>
+                <p className="productDesc">Silver 1.2ct Round Blue Diamond</p>
+              </a>
+              <a href="/" className="productItem productItem5">
+                <img
+                  className="productImg"
+                  src={img_product_ring_silver_round_blue_diamond}
+                  alt=""
+                />
+                <div className="productStage"></div>
+                <p className="productDesc">Silver 1.2ct Round Blue Diamond</p>
+              </a>
+            </Div__productRow>
+            <Div__productRow>
+              <a href="/" className="productItem productItem6">
+                <img
+                  className="productImg"
+                  src={img_product_ring_silver_round_blue_diamond}
+                  alt=""
+                />
+                <div className="productStage"></div>
+                <p className="productDesc">Silver 1.2ct Round Blue Diamond</p>
+              </a>
+              <a href="/" className="productItem productItem7">
                 <img
                   className="productImg"
                   src={img_product_ring_silver_round_blue_diamond}
@@ -345,7 +486,7 @@ const Block_one_row_jewellery = () => {
                 <p className="productDesc">Silver 1.2ct Round Blue Diamond</p>
               </a>
 
-              <a href="/" className="productItem">
+              <a href="/" className="productItem productItem8">
                 <img
                   className="productImg"
                   src={img_product_ring_silver_round_blue_diamond}
@@ -355,7 +496,119 @@ const Block_one_row_jewellery = () => {
                 <p className="productDesc">Silver 1.2ct Round Blue Diamond</p>
               </a>
 
-              <a href="/" className="productItem">
+              <a href="/" className="productItem productItem9">
+                <img
+                  className="productImg"
+                  src={img_product_ring_silver_round_blue_diamond}
+                  alt=""
+                />
+                <div className="productStage"></div>
+                <p className="productDesc">Silver 1.2ct Round Blue Diamond</p>
+              </a>
+
+              <a href="/" className="productItem productItem10">
+                <img
+                  className="productImg"
+                  src={img_product_ring_silver_round_blue_diamond}
+                  alt=""
+                />
+                <div className="productStage"></div>
+                <p className="productDesc">Silver 1.2ct Round Blue Diamond</p>
+              </a>
+            </Div__productRow>
+            <Div__productRow>
+              <a href="/" className="productItem productItem11">
+                <img
+                  className="productImg"
+                  src={img_product_ring_silver_round_blue_diamond}
+                  alt=""
+                />
+                <div className="productStage"></div>
+                <p className="productDesc">Silver 1.2ct Round Blue Diamond</p>
+              </a>
+
+              <a href="/" className="productItem productItem12">
+                <img
+                  className="productImg"
+                  src={img_product_ring_silver_round_blue_diamond}
+                  alt=""
+                />
+                <div className="productStage"></div>
+                <p className="productDesc">Silver 1.2ct Round Blue Diamond</p>
+              </a>
+
+              <a href="/" className="productItem productItem13">
+                <img
+                  className="productImg"
+                  src={img_product_ring_silver_round_blue_diamond}
+                  alt=""
+                />
+                <div className="productStage"></div>
+                <p className="productDesc">Silver 1.2ct Round Blue Diamond</p>
+              </a>
+
+              <a href="/" className="productItem productItem14">
+                <img
+                  className="productImg"
+                  src={img_product_ring_silver_round_blue_diamond}
+                  alt=""
+                />
+                <div className="productStage"></div>
+                <p className="productDesc">Silver 1.2ct Round Blue Diamond</p>
+              </a>
+
+              <a href="/" className="productItem productItem15">
+                <img
+                  className="productImg"
+                  src={img_product_ring_silver_round_blue_diamond}
+                  alt=""
+                />
+                <div className="productStage"></div>
+                <p className="productDesc">Silver 1.2ct Round Blue Diamond</p>
+              </a>
+            </Div__productRow>
+            <Div__productRow>
+              <a href="/" className="productItem productItem16">
+                <img
+                  className="productImg"
+                  src={img_product_ring_silver_round_blue_diamond}
+                  alt=""
+                />
+                <div className="productStage"></div>
+                <p className="productDesc">Silver 1.2ct Round Blue Diamond</p>
+              </a>
+
+              <a href="/" className="productItem productItem17">
+                <img
+                  className="productImg"
+                  src={img_product_ring_silver_round_blue_diamond}
+                  alt=""
+                />
+                <div className="productStage"></div>
+                <p className="productDesc">Silver 1.2ct Round Blue Diamond</p>
+              </a>
+
+              <a href="/" className="productItem productItem18">
+                <img
+                  className="productImg"
+                  src={img_product_ring_silver_round_blue_diamond}
+                  alt=""
+                />
+                <div className="productStage"></div>
+                <p className="productDesc">Silver 1.2ct Round Blue Diamond</p>
+              </a>
+
+              <a href="/" className="productItem productItem19">
+                <img
+                  className="productImg"
+                  src={img_product_ring_silver_round_blue_diamond}
+                  alt=""
+                />
+                <div className="productStage"></div>
+                <p className="productDesc">Silver 1.2ct Round Blue Diamond</p>
+              </a>
+
+              <a href="/" className="productItem productItem20">
                 <img
                   className="productImg"
                   src={img_product_ring_silver_round_blue_diamond}
