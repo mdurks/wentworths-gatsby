2ff16c608cb5862df9e79c08888aebda648611fd
mdurks <mdurks@gmail.com>
2021-07-21 21:27:04 +0100

    product in list - disable number_with_commas regex function to see if it fixs iOS issue

--- src/components/Product_in_list.js
+++ src/components/Product_in_list.js
@@ -161,7 +161,8 @@ const Styled_Img = styled.div`
 const Product = props => {
   //
   function number_with_commas(x) {
-    return x.toString().replace(/\B(?<!\.\d*)(?=(\d{3})+(?!\d))/g, ",")
+    // return x.toString().replace(/\B(?<!\.\d*)(?=(\d{3})+(?!\d))/g, ",")
+    return x
   }
   //
   let tl = gsap.timeline()
