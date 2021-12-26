6862cc31b3e848769dba35708e0eefa2594e6eee
mdurks <mdurks@gmail.com>
2021-12-26 17:10:10 +0000

    Folder tidyup - block full size image

--- src/components/block_full_size_image/block_full_size_image.js
+++ src/components/block_full_size_image/block_full_size_image.js
@@ -3,7 +3,7 @@ import { useEffect } from "react"
 import { graphql, useStaticQuery } from "gatsby"
 
 import GraphImg from "graphcms-image"
-import styled from "styled-components"
+import { Div__block_full_size_image } from "./block_full_size_image.styles"
 
 import { gsap, ScrollTrigger } from "gsap/all"
 
@@ -22,45 +22,6 @@ const pageQuery = graphql`
           width
         }
       }
-    }
-  }
-`
-
-const section_vertical_padding = "12vh"
-const section_horizontal_padding = "50px"
-const section_content_max_width = "1800px"
-
-const Div__block_full_size_image = styled.div`
-  padding: (${section_vertical_padding} / 2) ${section_horizontal_padding};
-  min-height: calc(100vh + ${section_vertical_padding});
-  position: relative;
-  overflow: hidden;
-
-  .graphcms-image-outer-wrapper {
-    position: absolute !important;
-    height: 100%;
-    width: 100%;
-  }
-
-  .Section__hero__backgroundImg {
-    position: absolute;
-    /*
-    top: 50%;
-    left: 50%;
-    transform: translate(-50%, -50%);
-    object-fit: cover;
-    */
-    width: 100%;
-    height: 100%;
-
-    div {
-      width: 100%;
-      height: 100%;
-    }
-
-    @media (min-width: 2500px) {
-      max-width: 2500px;
-      // width: $section_content_max_width;
     }
   }
 `
---
+++ src/components/block_full_size_image/block_full_size_image.styles.js
@@ -0,0 +1,41 @@
+import styled from "styled-components"
+
+const section_vertical_padding = "12vh"
+const section_horizontal_padding = "50px"
+const section_content_max_width = "1800px"
+
+export const Div__block_full_size_image = styled.div`
+  position: relative;
+  min-height: calc(100vh + ${section_vertical_padding});
+  overflow: hidden;
+  margin-bottom: 14px;
+  padding: (${section_vertical_padding} / 2) ${section_horizontal_padding};
+
+  .graphcms-image-outer-wrapper {
+    position: absolute !important;
+    height: 100%;
+    width: 100%;
+  }
+
+  .Section__hero__backgroundImg {
+    position: absolute;
+    /*
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+    object-fit: cover;
+    */
+    width: 100%;
+    height: 100%;
+
+    div {
+      width: 100%;
+      height: 100%;
+    }
+
+    @media (min-width: 2500px) {
+      max-width: 2500px;
+      // width: $section_content_max_width;
+    }
+  }
+`
