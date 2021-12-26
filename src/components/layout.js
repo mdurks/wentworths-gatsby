7fbf3ed5d67839c22a2a0353ae712b27f22904f7
mdurks <mdurks@gmail.com>
2021-12-26 21:09:13 +0000

    code cleanup

--- src/components/layout.js
+++ src/components/layout.js
@@ -7,12 +7,12 @@
 
 import React from "react"
 import PropTypes from "prop-types"
-import { useStaticQuery, graphql } from "gatsby"
+// import { useStaticQuery, graphql } from "gatsby"
 
 import { AppProvider } from "../store/AppContext"
 
 import SEO from "./layout/seo"
-import Header from "./layout/header"
+// import Header from "./layout/header"
 import MainNav from "./layout/MainNav"
 import Footer from "./layout/Footer"
 
@@ -26,15 +26,15 @@ import "../styles/style.css"
 
 const Layout = ({ children }) => {
   // const Layout = props => {
-  const data = useStaticQuery(graphql`
-    query SiteTitleQuery {
-      site {
-        siteMetadata {
-          title
-        }
-      }
-    }
-  `)
+  // const data = useStaticQuery(graphql`
+  //   query SiteTitleQuery {
+  //     site {
+  //       siteMetadata {
+  //         title
+  //       }
+  //     }
+  //   }
+  // `)
 
   return (
     <>
