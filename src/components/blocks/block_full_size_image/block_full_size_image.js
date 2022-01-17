5ac626eaf56cf4f982fd4d98654399fb7fd95f77
mdurks <mdurks@gmail.com>
2022-01-17 22:18:20 +0000

    block full size image - added tooltip link for desktop and link to product on mobile

--- src/components/blocks/block_full_size_image/block_full_size_image.js
+++ src/components/blocks/block_full_size_image/block_full_size_image.js
@@ -14,12 +14,19 @@ const pageQuery = graphql`
   {
     gcms {
       blockFullSizeImages {
-        images {
-          url
+        products {
           id
-          handle
-          height
-          width
+          name
+          slug
+          productType
+          categoryType
+          blockFullImageComponent {
+            id
+            url
+            width
+            height
+            handle
+          }
         }
       }
     }
@@ -31,42 +38,79 @@ const Block_one_row_jewellery = () => {
     gcms: { blockFullSizeImages },
   } = useStaticQuery(pageQuery)
 
+  console.log("blockFullSizeImages", blockFullSizeImages)
   let gsap__image = null
 
   useEffect(() => {
     // scrolling zoom effect on image
     gsap.from(".block_full_size_image", {
       scrollTrigger: {
-        trigger: gsap__image,
         // markers: true,
+        trigger: gsap__image,
         start: "top 112%",
-        scrub: 1,
+        scrub: true,
       },
       // duration: 2,
       scale: 1.3,
     })
 
-    // Mouse move hero image parallax
-    const mouseMoveParallaxTarget = document.querySelector(
-      ".block_full_size_image"
-    )
-    const mouseMoveParallaxTarget_xratio =
-      (mouseMoveParallaxTarget.offsetWidth - window.innerWidth) /
-      window.innerWidth
-    const mouseMoveParallaxTarget_yratio =
-      (window.innerHeight * 1.05 - window.innerHeight) / window.innerHeight
+    gsap.to(".block_full_size_image__descriptionWrapper", {
+      scrollTrigger: {
+        // markers: true,
+        trigger: gsap__image,
+        start: `top ${window.innerHeight - 150}`,
+        end: "bottom bottom",
+        scrub: 1,
+        // toggleClass: { targets: gsap__image, className: "fixDescription" },
 
-    const mouseMoveParallaxTargetFunc = e => {
-      // console.log(e.pageX, e.pageY - window.scrollY)
-      mouseMoveParallaxTarget.style.left =
-        e.pageX * mouseMoveParallaxTarget_xratio * -1 + "px"
-      mouseMoveParallaxTarget.style.top =
-        (e.pageY - window.scrollY) * mouseMoveParallaxTarget_yratio * -1 + "px"
+        onEnter: () => {
+          document
+            .querySelector(".block_full_size_imageWrapper")
+            .classList.add("fixDescription", "fixDescriptionFadeIn")
+        },
+        onLeaveBack: () => {
+          document
+            .querySelector(".block_full_size_imageWrapper")
+            .classList.remove("fixDescription", "fixDescriptionFadeIn")
+        },
+        onLeave: () => {
+          document
+            .querySelector(".block_full_size_imageWrapper")
+            .classList.remove("fixDescription")
+        },
+        onEnterBack: () => {
+          document
+            .querySelector(".block_full_size_imageWrapper")
+            .classList.add("fixDescription")
+        },
+      },
+      // y: window.innerHeight / 1.75,
+    })
+
+    if (window.innerWidth > 1024) {
+      // Mouse move hero image parallax
+      const mouseMoveParallaxTarget = document.querySelector(
+        ".block_full_size_image"
+      )
+      const mouseMoveParallaxTarget_xratio =
+        (mouseMoveParallaxTarget.offsetWidth - window.innerWidth) /
+        window.innerWidth
+      const mouseMoveParallaxTarget_yratio =
+        (window.innerHeight * 1.05 - window.innerHeight) / window.innerHeight
+
+      const mouseMoveParallaxTargetFunc = e => {
+        // console.log(e.pageX, e.pageY - window.scrollY)
+        mouseMoveParallaxTarget.style.left =
+          e.pageX * mouseMoveParallaxTarget_xratio * -1 + "px"
+        mouseMoveParallaxTarget.style.top =
+          (e.pageY - window.scrollY) * mouseMoveParallaxTarget_yratio * -1 +
+          "px"
+      }
+      mouseMoveParallaxTarget.addEventListener(
+        "mousemove",
+        mouseMoveParallaxTargetFunc
+      )
     }
-    mouseMoveParallaxTarget.addEventListener(
-      "mousemove",
-      mouseMoveParallaxTargetFunc
-    )
 
     // Show/Hide main nav when this component in central view
     gsap.from(".block_full_size_image", {
@@ -93,22 +137,62 @@ const Block_one_row_jewellery = () => {
 
   return (
     <>
-      <Div__block_full_size_image ref={e => (gsap__image = e)}>
+      <Div__block_full_size_image
+        className="block_full_size_imageWrapper"
+        ref={e => (gsap__image = e)}
+        onClick={() => {
+          if (typeof window !== "undefined" && window.innerWidth < 1024) {
+            location.href = `/${blockFullSizeImages[0].products[0].categoryType[0]}/${blockFullSizeImages[0].products[0].productType}/${blockFullSizeImages[0].products[0].slug}/`
+          }
+        }}
+      >
         {typeof window !== "undefined" && window.innerWidth < 600 ? (
           <GraphImg
             className="block_full_size_image"
-            image={blockFullSizeImages[0].images[1]}
+            image={
+              blockFullSizeImages[0].products[0].blockFullImageComponent[1]
+            }
             transforms={["quality=value:80"]}
             maxWidth={2000}
           />
         ) : (
           <GraphImg
             className="block_full_size_image"
-            image={blockFullSizeImages[0].images[0]}
+            image={
+              blockFullSizeImages[0].products[0].blockFullImageComponent[0]
+            }
             transforms={["quality=value:80"]}
             maxWidth={2000}
           />
         )}
+        <div className="block_full_size_image__descriptionWrapper">
+          {/* <span className="block_full_size_image__questionButon">?</span> */}
+          <a
+            href={`/${blockFullSizeImages[0].products[0].categoryType[0]}/${blockFullSizeImages[0].products[0].productType}/${blockFullSizeImages[0].products[0].slug}/`}
+            className="block_full_size_image__link"
+            onMouseOver={() => {
+              document.querySelector(
+                ".block_full_size_image__link"
+              ).style.width =
+                document.querySelector(
+                  ".block_full_size_image__questionTextInner"
+                ).offsetWidth +
+                40 +
+                "px"
+            }}
+            onMouseOut={() => {
+              document.querySelector(
+                ".block_full_size_image__link"
+              ).style.width = "44px"
+            }}
+          >
+            <span className="block_full_size_image__questionText">
+              <span className="block_full_size_image__questionTextInner">
+                {blockFullSizeImages[0].products[0].name}
+              </span>
+            </span>
+          </a>
+        </div>
       </Div__block_full_size_image>
     </>
   )
--- src/components/blocks/block_full_size_image/block_full_size_image.styles.js
+++ src/components/blocks/block_full_size_image/block_full_size_image.styles.js
@@ -1,4 +1,5 @@
 import styled, { css } from "styled-components"
+import css_breakpoints from "../../../common/css_breakpoints"
 
 const section_vertical_padding = "12vh" // add this value in the scrolltrigger start point
 const section_horizontal_padding = "50px"
@@ -36,6 +37,110 @@ export const Div__block_full_size_image = styled.div`
     @media (min-width: 2500px) {
       max-width: 2500px;
       // width: $section_content_max_width;
+    }
+  }
+
+  &.fixDescription .block_full_size_image__descriptionWrapper {
+    position: fixed;
+  }
+
+  &.fixDescriptionFadeIn .block_full_size_image__descriptionWrapper {
+    opacity: 1;
+  }
+
+  .block_full_size_image__descriptionWrapper {
+    display: none;
+
+    ${css_breakpoints.min_tablet} {
+      display: block;
+      position: absolute;
+      bottom: 25px;
+      right: 25px;
+      overflow: hidden;
+      opacity: 0;
+      transition: opacity ease .4s;
+      bottom: 50px;
+      right: 50px;
+    }
+
+    .block_full_size_image__questionButon {
+      /* position: absolute;
+      top: 0;
+      right: 0; */
+      display: block;
+      width: 44px;
+      height: 44px;
+      padding: 10px;
+      font-size:  25px;
+      font-weight: bold;
+      color: #b3924c;
+      background: white;
+      border-radius: 100%;
+      line-height: 0;
+      text-align: center;
+      transition: all ease .s;
+      z-index: 1;
+      pointerevents: none;
+
+      ${css_breakpoints.min_tablet} {
+        display: none;
+      }
+    }
+
+    a {
+      display: none;
+      width: 44px;
+      height: 44px;
+      background: hsl(0deg 0% 100% / 80%);
+      border-radius: 100px;
+      overflow: hidden;
+      transition: all ease .4s;
+
+      ${css_breakpoints.min_tablet} {
+        display: block;
+      }
+
+      :before {
+        content: '?';
+        position: absolute;
+        top: 0;
+        right: 0;
+        width: 44px;
+        height: 44px;
+        padding: 10px;
+        font-size:  25px;
+        font-weight: bold;
+        color: #b3924c;
+        background: white;
+        border-radius: 100%;
+        line-height: 23px;
+        text-align: center;
+        opacity: 1;
+        transition: all ease .3s;
+      }
+
+      > span {
+        position: absolute;
+        top: 50%;
+        left: 20px;
+        transform: translateY(-50%);
+        width: 500px;
+        opacity: 0;
+        transition: all ease .4s;
+        text-decoration: underline;
+      }
+
+      @media (hover: hover) {
+        &:hover {
+          :before {
+            opacity: 0;
+          }
+
+          > span {
+            opacity: 1;
+          }
+        }
+      }
     }
   }
 `
