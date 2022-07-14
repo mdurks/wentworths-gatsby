1145b262ce4d3d39c1180c8ea6e0d76f11860a40
mdurks <mdurks@gmail.com>
2022-07-14 22:03:56 +0100

    Added smooth scroll js via CDN.  Added test page for natural diamond search iFrame which is not showing anything.

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -84,5 +84,9 @@ export const onRenderBody = (
       src="https://chimpstatic.com/mcjs-connected/js/users/d5846f86c03e5c2c5a3e185f8/02028c8b0af713f275541bbbb.js"
       defer
     ></script>,
+    <script
+      defer
+      src="https://cdnjs.cloudflare.com/ajax/libs/smoothscroll/1.4.10/SmoothScroll.min.js"
+    ></script>,
   ])
 }
